b2a554e29394569bcab047142dd178480c019cd8
philip <philip.dossantos@hotmail.com>
2021-03-02 21:40:36 -0500

    some playing around. got a lamp block now i guess.

--- src/main/java/philip83/looseends/blocks/TestBlock.java
+++ src/main/java/philip83/looseends/blocks/TestBlock.java
@@ -2,28 +2,55 @@ package philip83.looseends.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.util.Direction;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-
+import net.minecraft.world.World;
+import philip83.looseends.LooseEnds;
 
 public class TestBlock extends Block {
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
+
     public TestBlock(Properties properties) {
         super(properties);
+        this.setDefaultState(this.getDefaultState().with(POWERED, false));
     }
 
     @Override
-    public int getWeakPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
-        return 15;
+    public int getLightValue(BlockState state, IBlockReader world, BlockPos pos) {
+        return state.get(POWERED) ? 15 : 0;
     }
 
     @Override
-    public int getStrongPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
-        return 15;
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+
+        builder.add(POWERED);
     }
 
+
+    public void toggleState(BlockState state, World world, BlockPos pos) {
+        Boolean isPowered = state.get(POWERED);
+        world.setBlockState(pos, state.with(POWERED, !isPowered));
+
+        world.notifyNeighborsOfStateChange(pos, this);
+    }
+
+
     @Override
-    public boolean canProvidePower(BlockState state) {
-        return true;
+    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+        if (!worldIn.isRemote) {
+            boolean flag = state.get(POWERED);
+            if (flag != worldIn.isBlockPowered(pos)) {
+                toggleState(state,worldIn,pos);
+            }
+
+        }
+
+            LooseEnds.LOGGER.debug("Poke.");
+
     }
 }
+
+
