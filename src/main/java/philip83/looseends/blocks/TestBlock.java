0ae860964f7e8346fbb5b55a0fcc581b8626a114
philip <philip.dossantos@hotmail.com>
2021-03-10 17:25:19 -0500

    i think this is the final version

--- src/main/java/philip83/looseends/blocks/TestBlock.java
+++ src/main/java/philip83/looseends/blocks/TestBlock.java
@@ -5,11 +5,10 @@ import net.minecraft.block.BlockState;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
-import philip83.looseends.LooseEnds;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -33,14 +32,6 @@ public class TestBlock extends Block {
 
         builder.add(POWERED).add(TRIGGERED);
     }
-
-    @Override
-    public void onPlayerDestroy(IWorld worldIn, BlockPos pos, BlockState state) {
-        super.onPlayerDestroy(worldIn, pos, state);
-    }
-
-
-
 
     private void safeBreak(BlockState state, World world, BlockPos pos){
         Set<BlockPos> toActivate = new HashSet<>();
@@ -81,37 +72,15 @@ public class TestBlock extends Block {
     private void updateTouching(BlockPos pos, World world,  Set<BlockPos> previous){
         BlockPos temp;
 
-        temp = pos.down();
-        if(world.getBlockState(temp).getBlock() == this.getBlock() && !previous.contains(temp)){
-            previous.add(temp);
-            updateTouching(temp, world, previous);
-        }
-        temp = pos.up();
-        if(world.getBlockState(temp).getBlock() == this.getBlock() && !previous.contains(temp)){
-            previous.add(temp);
-            updateTouching(temp, world, previous);
-        }
-        temp = pos.north();
-        if(world.getBlockState(temp).getBlock() == this.getBlock() && !previous.contains(temp)){
-            previous.add(temp);
-            updateTouching(temp, world, previous);
-        }
-        temp = pos.south();
-        if(world.getBlockState(temp).getBlock() == this.getBlock() && !previous.contains(temp)){
-            previous.add(temp);
-            updateTouching(temp, world, previous);
-        }
-        temp = pos.east();
-        if(world.getBlockState(temp).getBlock() == this.getBlock() && !previous.contains(temp)){
-            previous.add(temp);
-            updateTouching(temp, world, previous);
-        }
-        temp = pos.west();
-        if(world.getBlockState(temp).getBlock() == this.getBlock() && !previous.contains(temp)){
-            previous.add(temp);
-            updateTouching(temp, world, previous);
-        }
+        Direction[] directions = Direction.values();
 
+        for(Direction direction: directions){
+            temp = pos.offset(direction);
+            if(world.getBlockState(temp).getBlock() == this.getBlock() && !previous.contains(temp)){
+                previous.add(temp);
+                updateTouching(temp, world, previous);
+            }
+        }
     }
 
     @Override
